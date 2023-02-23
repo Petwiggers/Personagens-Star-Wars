007d93e18288f2e5ae2972b23e3cd43e11040d88
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-02-23 11:28:21 -0300

    Inicio do Projeto e injecao de componente Star Wars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StarWarsComponent } from "../Componests/star-wars/star-wars.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarWarsComponent
   ],
   imports: [
     BrowserModule,
